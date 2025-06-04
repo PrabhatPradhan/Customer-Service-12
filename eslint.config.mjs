e40057d5f761c7f4ca5464c755de254ea824a65c
Prabhat <mmrprabhujii@gmail.com>
2025-06-04 13:56:49 +0530

    chenge rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add your custom rules as an object
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
